9754c82b8a6e25b484cfe4cdb95f5ca79e8cbeb0
Eluda <111eluda111@gmail.com>
2022-08-08 11:42:05 +0100

    Configure Eslint, Prettier, Tailwind, Fonts, Fontawesome, Icons and SEO.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+}
